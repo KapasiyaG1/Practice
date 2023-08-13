8d8a1001f45f650d093d4c2cd8d961a14e2f4958
soyam kapasiya <soyamp232@gmail.com>
2023-08-13 15:02:37 +0530

    secont

--- NewClass8.java
+++ NewClass8.java
@@ -5,14 +5,48 @@
  */
 package Practice;
 
+import java.util.Scanner;
+
 /**
  *
  * @author soyam
  */
 public class NewClass8 
 {
+    public static int[] plusOne(int[] digits) 
+    {
+       for (int i = digits.length - 1; i >= 0; i--) 
+       {
+	        if (digits[i] < 9) 
+            {
+	    	    digits[i]++;
+	    	    return digits;
+	        }
+	        digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+    
     public static void main(String[] args) 
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int size = scan.nextInt();
+        int[] arr = new int[size];
         
+        for(int i=0;i<size;i++)
+        {
+            System.out.print("Enter element "+(i+1)+": ");
+            arr[i]=scan.nextInt();
+        }
+        int[] ans;
+        ans = plusOne(arr);
+        
+        for(int val:ans)
+        {
+            System.out.print(val+" ");
+        }
     }
 }
